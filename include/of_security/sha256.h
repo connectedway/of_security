2007b45099049c85a1fbc2029bef70baba99432c
Richard Schmitt <rschmitt@bluepeach.com>
2022-06-20 19:06:01 -0500

    expose sha256 vector routine

--- include/of_security/sha256.h
+++ include/of_security/sha256.h
@@ -7,6 +7,9 @@
 extern "C"
 {
 #endif
+  OFC_INT hmac_sha256_vector(const OFC_UCHAR *key, OFC_SIZET key_len, 
+                             OFC_SIZET num_elem, const OFC_UCHAR *addr[], 
+                             const OFC_SIZET *len, OFC_UCHAR *mac);
   OFC_INT hmac_sha256(const OFC_UCHAR *key, OFC_SIZET key_len, 
 		       const OFC_UCHAR *data, OFC_SIZET data_len, 
 		       OFC_UCHAR *mac) ;
